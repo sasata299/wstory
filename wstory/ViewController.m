82e48e32797f44e0e40efd099f5f1fd8a8261d77
sasata299 <karaage299@gmail.com>
2013-12-06 07:53:08 +0900

    Create table view by hitting the api

--- wstory/ViewController.m
+++ wstory/ViewController.m
@@ -6,10 +6,12 @@
 //  Copyright (c) 2013年 sasata299. All rights reserved.
 //
 
+#import <SVProgressHUD/SVProgressHUD.h>
+#import <AFNetworking/AFNetworking.h>
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSArray *responseData;
 @end
 
 @implementation ViewController
@@ -17,13 +19,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeBlack];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:@"https://qiita.com/api/v1/users/sasata299/stocks" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        self.responseData = responseObject;
+        [self.tableView reloadData];
+        [SVProgressHUD dismiss];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section
+{
+    return [self.responseData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    if (self.responseData) {
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.textLabel.text = self.responseData[indexPath.row][@"title"];
+    }
+    return cell;
 }
 
 @end
